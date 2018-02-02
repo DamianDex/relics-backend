dbd2a1094643787334cd7003a665ff4590f7405e
farad <damian.fara@pega.com>
2018-02-02 11:28:11 +0100

    Initial commit

---
+++ src/main/java/com/relics/backend/relicsbackend/RelicsBackendApplication.java
@@ -0,0 +1,12 @@
+package com.relics.backend.relicsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelicsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelicsBackendApplication.class, args);
+	}
+}

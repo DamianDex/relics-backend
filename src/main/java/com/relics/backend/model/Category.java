d55f79176233087d896be3731b556bd89ee307da
farad <damian.fara@pega.com>
2018-02-08 13:05:57 +0100

    Add Category entity to model.

---
+++ src/main/java/com/relics/backend/model/Category.java
@@ -0,0 +1,27 @@
+package com.relics.backend.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public Long id;
+
+    public String categoryName;
+
+    public Category() {
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+}
--- src/main/java/com/relics/backend/model/Relic.java
+++ src/main/java/com/relics/backend/model/Relic.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import java.util.Set;
 
 @Entity
@@ -19,7 +20,9 @@ public class Relic {
     public String voivodeshipName;
     public Double latitude;
     public Double longitude;
-    public Set<String> categories;
+
+    @OneToMany
+    public Set<Category> categories;
 
     public Relic() {
     }

d1e16107a9dd4f7f8c10f5ac63b938daec138d74
farad <damian.fara@pega.com>
2018-02-08 10:16:03 +0100

    Simple methods in controller to add relics and display all of them. Sucessfull connection with postgres db.

--- src/main/java/com/relics/backend/controller/RelicController.java
+++ src/main/java/com/relics/backend/controller/RelicController.java
@@ -1,10 +1,17 @@
 package com.relics.backend.controller;
 
+import com.relics.backend.model.Relic;
 import com.relics.backend.repository.RelicRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -13,8 +20,14 @@ public class RelicController {
     @Autowired
     private RelicRepository relicRepository;
 
+    @PostMapping("/relics")
+    public void createNewRelic(@Valid @RequestBody Relic relic) {
+        relicRepository.save(relic);
+    }
+
     @GetMapping("/relics")
-    public void getAllRelics() {
-        System.out.println("Here");
+    @ResponseBody
+    public List<Relic> getAllRelics() {
+        return relicRepository.findAll();
     }
 }

8db6d90e04556a38ca8dd29821419b489036fdbd
farad <damian.fara@pega.com>
2018-02-14 11:10:14 +0100

    Stub implementation of recommender

---
+++ src/main/java/com/relics/backend/recommender/Recommender.java
@@ -0,0 +1,46 @@
+package com.relics.backend.recommender;
+
+import java.io.File;
+import java.util.List;
+
+import org.apache.mahout.cf.taste.impl.model.GenericDataModel;
+import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
+import org.apache.mahout.cf.taste.impl.neighborhood.ThresholdUserNeighborhood;
+import org.apache.mahout.cf.taste.impl.recommender.GenericUserBasedRecommender;
+import org.apache.mahout.cf.taste.impl.similarity.PearsonCorrelationSimilarity;
+
+import org.apache.mahout.cf.taste.model.DataModel;
+import org.apache.mahout.cf.taste.neighborhood.UserNeighborhood;
+
+import org.apache.mahout.cf.taste.recommender.RecommendedItem;
+import org.apache.mahout.cf.taste.recommender.UserBasedRecommender;
+
+import org.apache.mahout.cf.taste.similarity.UserSimilarity;
+
+
+// https://www.slideshare.net/Cataldo/tutoria-mahout-recommendation
+public class Recommender {
+    public static void main(String args[]){
+        try{
+            //Creating data model
+            DataModel datamodel = new FileDataModel(new File("aa"));
+
+            //Creating UserSimilarity object.
+            UserSimilarity usersimilarity = new PearsonCorrelationSimilarity(datamodel);
+
+            //Creating UserNeighbourHHood object.
+            UserNeighborhood userneighborhood = new ThresholdUserNeighborhood(3.0, usersimilarity, datamodel);
+
+            //Create UserRecomender
+            UserBasedRecommender recommender = new GenericUserBasedRecommender(datamodel, userneighborhood, usersimilarity);
+
+            List<RecommendedItem> recommendations = recommender.recommend(2, 3);
+
+            for (RecommendedItem recommendation : recommendations) {
+                System.out.println(recommendation);
+            }
+
+        }catch(Exception e){}
+
+    }
+}

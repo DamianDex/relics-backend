84ea81327c1bced1a57bb3972abb18a6d5bbc319
Sławek <sla.gladys@gmail.com>
2018-04-08 18:20:08 +0200

    Change adding Images to Relic

--- src/main/java/com/relics/backend/model/Image.java
+++ src/main/java/com/relics/backend/model/Image.java
@@ -1,10 +1,6 @@
 package com.relics.backend.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 
 @Entity
 public class Image {
@@ -18,8 +14,33 @@ public class Image {
     public String pathToImage;
 
     @ManyToOne
+    //@JoinColumn(name="relic_id")
     public Relic relic;
 
     public Image() {
+    }
+
+    public boolean isMainImage() {
+        return isMainImage;
+    }
+
+    public void setMainImage(boolean mainImage) {
+        isMainImage = mainImage;
+    }
+
+    public String getPathToImage() {
+        return pathToImage;
+    }
+
+    public void setPathToImage(String pathToImage) {
+        this.pathToImage = pathToImage;
+    }
+
+    public Relic getRelic() {
+        return relic;
+    }
+
+    public void setRelic(Relic relic) {
+        this.relic = relic;
     }
 }
--- src/main/java/com/relics/backend/model/Relic.java
+++ src/main/java/com/relics/backend/model/Relic.java
@@ -5,15 +5,7 @@ import com.relics.backend.View;
 import org.hibernate.annotations.Cascade;
 import org.hibernate.annotations.CascadeType;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToMany;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
-import javax.persistence.SequenceGenerator;
+import javax.persistence.*;
 import java.util.Set;
 
 @Entity
@@ -50,6 +42,8 @@ public class Relic {
     public Set<Review> reviews;
 
     @OneToMany
+    //@JoinColumn(name="relic_id")
+    @Cascade(CascadeType.PERSIST)
     public Set<Image> images;
 
     public Relic() {
